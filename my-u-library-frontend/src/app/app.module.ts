ab45cce5c33c2dd70b666829c62f5f1fd4605f52
L3X7 <lexmelara@gmail.com>
2025-11-30 00:32:48 -0600

    feat: Application redesign.

--- my-u-library-frontend/src/app/app.module.ts
+++ my-u-library-frontend/src/app/app.module.ts
@@ -2,26 +2,18 @@ import { NgModule } from '@angular/core';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './modules/home/home.component';
-import { FooterModule } from './shared/footer/footer.module';
-// import { NavbarModule } from './shared/navbar/navbar.module';
-import { SidebarModule } from './shared/sidebar/sidebar.module';
-import { Error404Component } from './shared/pages/error404/error404.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        HomeComponent,
-        Error404Component,
+        AppComponent
     ],
     exports: [],
     bootstrap: [AppComponent],
     imports: [
         ReactiveFormsModule,
         AppRoutingModule,
-        SidebarModule,
-        FooterModule,],
+    ],
     providers: [provideHttpClient(withInterceptorsFromDi())]
 })
 export class AppModule { }
