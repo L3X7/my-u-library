7ecb38d72a8ed1adda36304cfbac3b754a426d7d
L3X7 <lexmelara@gmail.com>
2025-11-29 22:51:49 -0600

    feat: Migration to angular 16

--- my-u-library-frontend/src/app/guards/auth.guard.ts
+++ my-u-library-frontend/src/app/guards/auth.guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { SecurityService } from '../services/security.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
     constructor(public router: Router, private autService: SecurityService) { }
 
